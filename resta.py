c533256e088d007699ce151ce91779e1a0653646
PatricioVargasR <1722110158@utectulancingo.edu.mx>
2025-06-13 15:20:13 -0600

    Se añadió las operaciones faltantes

---
+++ resta.py
@@ -0,0 +1,6 @@
+primer_valor = float(input("Ingresa tu primer número: "))
+segundo_valor = float(input("Ingresa tu segundo número: "))
+
+resultado = primer_valor - segundo_valor
+
+print(f"El resultado de la resta es: {resultado}")
